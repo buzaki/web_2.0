a6947118c138d5e23c53363ba95b3cf4728e3a0e
root <admin@id3m.net>
2016-10-14 02:11:53 +0530

    contact form

--- php/contact.php
+++ php/contact.php
@@ -1,46 +1,105 @@
+<?php
+
+$error = "";
+$done = "";
+if ($_POST) {
+    
+    $error = "";
+    
+    if (!$_POST['email']) {
+        
+        $error .= "an email address is required<br>";
+    }
+    
+    
+    if (!$_POST['subject']) {
+        
+        $error .= "the subject is required<br>";
+    }
+    
+    
+    if (!$_POST['content']) {
+        
+        $error .= "the content is required";
+    }
+    
+    if ( $_POST['email'] && filter_var($_POST['email'], FILTER_VALIDATE_EMAIL) === false) {
+
+       $error .= "an email address is invalid";
+}
+    
+    if ($error != "" ) {
+        
+        $error = "<div class='alert alert-danger' role='alert'><strong><p>Faild is Missing :</strong><br>" . $error . "</p></div>";
+    } else {
+        
+        $emailTo = "admin@id3m.net";
+        
+        $subject = $_POST['subject'];
+        
+        $content = $_POST['content'];
+        
+        $headers = "From: ".$_POST['email'];
+        
+       
+        
+        
+        if (mail($emailTo, $subject, $content, $headers)) {
+            
+                 $done = "<div class='alert alert-success' role='alert'><p>You successfully drop us your message</p></div>";
+            
+        }else {
+            
+           $error = "<div class='alert alert-danger' role='alert'><p>your message can't be send right now :(</p></div>";
+            
+        }
+        
+  
+    }
+
+}
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
   <head>
-    <!-- Required meta tags always come first -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
-
-    <!-- Bootstrap CSS -->
-    <style>
-
-  #container {
-    padding: 20px;
-    width: 80%;
-
-    }</style>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.4/css/bootstrap.min.css" integrity="2hfp1SzUoho7/TsGGGDaFdsuuDL0LX2hnUp6VkX3CUQ2K4K+xjboZdsXyp4oUHZj" crossorigin="anonymous">
+<style type="text/css">
+    #container {
+        margin: 0 auto;
+        width: 80%;
+        
+    }
+      
+      </style>
+    <link rel="stylesheet" href="bootstrap.min.css">
   </head>
   <body>
       <div id="container">
     <h1>Get in touch!</h1>
-
-<div id="cc">
-
-    <form>
+<div id="error_div"><? echo $error.$done; ?></div>
+    <form method="post">
 
 
       <div class="form-group">
         <label for="email">Email address</label>
-        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+        <input type="email" name="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Enter email">
         <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
       </div>
 
 
       <div class="form-group">
         <label for="subject">Subject</label>
-        <input type="text" class="form-control" id="Subject">
+        <input type="text" name="subject" class="form-control" id="subject">
       </div>
 
 
       <div class="form-group">
         <label for="content">what you want ?</label>
-        <textarea class="form-control" id="content" rows="3"></textarea>
+        <textarea name="content" class="form-control" id="content" rows="3"></textarea>
       </div>
 
 
@@ -49,21 +108,53 @@
 
 
       </div>
-    </div>
+      
+
 
 
     <!-- jQuery first, then Tether, then Bootstrap JS. -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.0.0/jquery.min.js" integrity="sha384-THPy051/pYDQGanwU6poAc/hOdQxjnOEXzbT+OuUAFqNqFjL+4IGLBgCJC3ZOShY" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.2.0/js/tether.min.js" integrity="sha384-Plbmg8JY28KFelvJVai01l8WyZzrYWG825m+cZ0eDDS1f7d/js6ikvy1+X+guPIB" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.4/js/bootstrap.min.js" integrity="VjEeINv9OSwtWFLAtmc4JCtEJXXBub00gtSnszmspDLCtC0I4z4nqz7rEFbIZLLU" crossorigin="anonymous"></script>
+    <script src="jquery.min.js" ></script>
+    <script src="tether.min.js" ></script>
+    <script src="bootstrap.min.js" ></script>
 
 <script type="text/javascript">
 
 $("form").submit(function (e) {
-      e.preventDefault(
+    
+      e.preventDefault()
+    
+    var error = "";
+    
+        if ( $("#email").val() == "" ) {
+        
+        error += "The Email Faild is Missing</br>"
+        
+    }
+    
+    if ( $("#subject").val() == "" ) {
+        
+        error += "The Subject Faild is Missing </br>"
+         
+    }
+    
+    if ( $("#content").val() == "" ) {
+        
+        error += "The Content Faild is Missing "
 
+    }
+    
 
-}
+    if (error != "") {
+        
+        $("#error_div").html('<div class="alert alert-danger" role="alert"><strong><p>Faild is Missing :</strong><br>' + error + '</p></div>');
+    } else {
+        
+               $("form").unbind('submit').submit();
+        
+    }
+        
+
+});
 
 </script>
 
