d88b95d4cfc5bbc1d38f7c8e3864a884daa122ca
root <admin@id3m.net>
2016-10-16 01:47:50 +0530

    MySQL files

---
+++ MySQL/new.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: vksood
+ * Date: 10/16/16
+ * Time: 12:56 AM
+ */
--- php/weather-scraper2.php
+++ php/weather-scraper2.php
@@ -1,47 +1,51 @@
 <?php
-    
+
     $weather = "";
     $error = "";
-    
+
     if ($_GET['city']) {
-        
+
         $city = str_replace(' ', '', $_GET['city']);
-        
+
+        if(any zeft){
+
+
+        }
+
         $file_headers = @get_headers("http://www.weather-forecast.com/locations/".$city."/forecasts/latest");
-        
-        
+
         if($file_headers[0] == 'HTTP/1.1 404 Not Found') {
-    
+
             $error = "That city could not be found.";
 
         } else {
-        
+
         $forecastPage = file_get_contents("http://www.weather-forecast.com/locations/".$city."/forecasts/latest");
-        
+
         $pageArray = explode('3 Day Weather Forecast Summary:</b><span class="read-more-small"><span class="read-more-content"> <span class="phrase">', $forecastPage);
             
         if (sizeof($pageArray) > 1) {
-        
+
                 $secondPageArray = explode('</span></span></span>', $pageArray[1]);
-            
+
                 if (sizeof($secondPageArray) > 1) {
 
                     $weather = $secondPageArray[0];
-                    
+
                 } else {
-                    
+
                     $error = "That city could not be found.";
-                    
+
                 }
-            
+
             } else {
-            
+
                 $error = "That city could not be found.";
-            
+
             }
-        
+
         }
-        
+
     }
 
 
@@ -57,37 +61,37 @@
           <link rel="stylesheet" href="bootstrap.min.css">
 
 <style type="text/css">
-    
-    html { 
-        
-  background: url(bg.jpg) no-repeat center center fixed; 
+
+    html {
+
+  background: url(bg.jpg) no-repeat center center fixed;
   -webkit-background-size: cover;
   -moz-background-size: cover;
   -o-background-size: cover;
   background-size: cover;
 }
-    
+
 
 
     body {
-        
+
         background: none;
         color: aliceblue;
     }
-    
+
     input {
-        
+
         margin: 20px 0;
     }
     .container {
-        
+
         text-align: center;
         margin-top: 200px;
         width: 450px;
     }
-    
+
     #weather {
-        
+
         padding: 10px;
     }
       </style>
@@ -98,7 +102,7 @@
 
       <h1>What's the Weather?</h1>
           </div>
-          
+
 
           <form>
   <div class="form-group">
@@ -108,73 +112,73 @@
   <button type="submit" class="btn btn-primary">Search :) </button>
 </form>
 
-          
-          <div id="weather"><?php 
-              
+
+          <div id="weather"><?php
+
               if ($weather) {
-                  
+
                   echo '<div class="alert alert-success" role="alert">
   '.$weather.'
 </div>';
-                  
+
               } else if ($error) {
-                  
+
                   echo '<div class="alert alert-danger" role="alert">
   '.$error.'
 </div>';
-                  
+
               }
-              
+
               ?></div>
       </div>
-          
+
           <div id="error_div"></div>
 
-          
-          
-          
-          
-          
-          
-          
+
+
+
+
+
+
+
       </div>
     <script src="jquery.min.js" ></script>
     <script src="tether.min.js" ></script>
     <script src="bootstrap.min.js" ></script>
-      
-    
+
+
       <script type="text/javascript">
-      
-      
+
+
           $("form").submit(function (e) {
-    
-    
+
+
         var error = "";
-    
+
         if ( $("#city").val() == "" ) {
-        
+
         error += "City Faild cannot be empty !</br>"
-        
+
     }
-    
+
     if (error != "") {
-        
+
         $("#error_div").html('<div class="alert alert-danger" role="alert"><strong><p>Faild is Missing :</strong><br>' + error + '</p></div>');
-        
+
         return false;
-        
+
     } else {
-        
-  
+
+
         return true;
-    
+
     }
-        
+
 
 });
-          
+
       </script>
-      
-      
+
+
   </body>
 </html>
