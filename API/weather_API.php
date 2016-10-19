947621dd13cb926d407a9fc37059cb9c9decca60
root <admin@id3m.net>
2016-10-19 13:23:51 +0530

    title

--- API/weather_API.php
+++ API/weather_API.php
@@ -48,7 +48,7 @@
                 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
                 <meta http-equiv="x-ua-compatible" content="ie=edge">
 
-                <title>Weather Scraper</title>
+                <title>Weather Finder</title>
                 <!-- Bootstrap CSS -->
                 <link rel="stylesheet" href="../php/bootstrap.min.css">
 
